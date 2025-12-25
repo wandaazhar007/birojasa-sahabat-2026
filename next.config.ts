065ea3f430d0560922f6d24db34f015be76de530
wanda azhar <wandaazhar@gmail.com>
2025-12-25 03:07:32 -0700

    add 301 redirect new pattttttttth

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/services",
+        destination: "/layanan",
+        permanent: true, // = 301
+      },
+      {
+        source: "/about",
+        destination: "/tentang",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "/kontak",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
